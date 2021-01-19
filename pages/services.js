e25aa0587e0c08296bb19e6609a50ca437dd2d41
Prince Demawo <mawondep@gmail.com>
2021-01-19 17:06:40 +0200

    add services page

---
+++ pages/services.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+const Services = () => {
+    return (
+        <div style={{minHeight: '100vh'}}>
+            <h1> Services</h1>
+        </div>
+    )
+}
+
+
+export default Services
